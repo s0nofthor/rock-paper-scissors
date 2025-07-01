d585abc85b1e374b2bc4dcd25bccb3a52c622ac4
s0nofthor <larsthomast@gmail.com>
2025-07-01 16:08:08 -0400

    Files formatted and outline added
    Files formatted and .js file now contains summary of what needs to be done.

---
+++ rock-paper-scissors.js
@@ -0,0 +1,7 @@
+//Have computer roll a random number between 0 and 3
+//Take the number and assign it to a string. 0 through 0.99 is rock, 1 through 1.99 is paper, and 2 through 2.99 is scissors
+//Log the user's choice of either rock, paper, or scissors
+//Create a scoreboard for the computer and user
+//Compare the computer and user's choices to see who wins the round
+//Give the winner a point on the scoreboard
+//Set full game to play for 5 rounds
