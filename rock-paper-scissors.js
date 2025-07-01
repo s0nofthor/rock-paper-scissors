51c4f46696f5fcdb3f36dfbecf88854e34d2814c
s0nofthor <larsthomast@gmail.com>
2025-07-01 16:44:53 -0400

    Assign choice values to number generated
    Need output to be non-numerical and either rock, paper, or scissors.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,7 +1,19 @@
 //Have computer roll a random number between 0 and 3
 let computerRoll = Math.random() * 3;
-
-//Take the number and assign it to a string. 0 through 0.99 is rock, 1 through 1.99 is paper, and 2 through 2.99 is scissors
+console.log(computerRoll);
+//Take the number and assign it to a string inside a function. 0 through 0.99 is rock, 1 through 1.99 is paper, and 2 through 2.99 is scissors
+function getComputerChoice() {
+    let result;
+    if (computerRoll < 1) {
+        result = "Rock";
+    } else if (computerRoll >= 1 && computerRoll < 2) {
+        result = "Paper";
+    } else {
+        result = "Scissors";
+    }
+    return result;
+}
+console.log(getComputerChoice());
 //Log the user's choice of either rock, paper, or scissors
 //Create a scoreboard for the computer and user
 //Compare the computer and user's choices to see who wins the round
