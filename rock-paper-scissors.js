f397e0f38ddb4ec32a2ca53882cf04daa5595812
s0nofthor <larsthomast@gmail.com>
2025-07-01 17:21:46 -0400

    Reformat user capitalization to match computer's
    Must be the same format to be compared easily.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,14 +2,14 @@
 let humanChoice = prompt("Rock, paper, or scissors?", "");
 
 //Have computer roll a random number between 0 and 3
-let computerRoll = Math.random() * 3;
+let computerChoice = Math.random() * 3;
 
 //Take the number and assign it to a string inside a function. 0 through 0.99 is rock, 1 through 1.99 is paper, and 2 through 2.99 is scissors
 function getComputerChoice() {
     let result;
-    if (computerRoll < 1) {
+    if (computerChoice < 1) {
         result = "Rock";
-    } else if (computerRoll >= 1 && computerRoll < 2) {
+    } else if (computerChoice >= 1 && computerChoice < 2) {
         result = "Paper";
     } else {
         result = "Scissors";
@@ -20,7 +20,7 @@ console.log(getComputerChoice());
 //Log the user's choice of either rock, paper, or scissors
 
 function getHumanChoice() {
-    return humanChoice;
+    return humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
 }
 console.log(getHumanChoice());
 //Create a scoreboard for the computer and user
@@ -28,6 +28,7 @@ let humanScore = 0;
 let computerScore = 0;
 console.log(humanScore);
 console.log(computerScore);
+
 //Compare the computer and user's choices to see who wins the round
 //Give the winner a point on the scoreboard
 //Set full game to play for 5 rounds
