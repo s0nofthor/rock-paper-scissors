1df15667d96297b8cb8478829c0bd0e90c99d003
s0nofthor <larsthomast@gmail.com>
2025-07-06 00:47:27 -0400

    Give a point to who earned it
    The scoreboard can now go up, making it an actual scoreboard

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -18,18 +18,16 @@ function getComputerChoice() {
 }
 console.log(getComputerChoice());
 //Log the user's choice of either rock, paper, or scissors
-
 function getHumanChoice() {
     return humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
 }
 console.log(getHumanChoice());
+
 //Create a scoreboard for the computer and user
 let humanScore = 0;
 let computerScore = 0;
-console.log(humanScore);
-console.log(computerScore);
 
-//Compare the computer and user's choices to see who wins the round
+//Compare the computer and user's choices to see who wins the round and give them the point
 function playRound(humanChoice, computerChoice) {
     if ((getHumanChoice() === "Rock" && getComputerChoice() === "Paper") 
         || (getHumanChoice() === "Paper" && getComputerChoice() === "Scissors") 
@@ -39,10 +37,19 @@ function playRound(humanChoice, computerChoice) {
         || (getHumanChoice() === "Paper" && getComputerChoice() === "Rock") 
         || (getHumanChoice() === "Scissors" && getComputerChoice() === "Paper")) {
         result = `You win! ${getHumanChoice()} beats ${getComputerChoice()}`;
+    } else {
+        result = `It's a tie!`
     }
     return result;
-}
+};
 console.log(playRound());
 
 //Give the winner a point on the scoreboard
+if (playRound() === `You win! ${getHumanChoice()} beats ${getComputerChoice()}`) {
+    humanScore += 1;
+} else if (playRound() === `You lose! ${getComputerChoice()} beats ${getHumanChoice()}`) {
+    computerScore += 1;
+}
+console.log(`User: ${humanScore} Bot: ${computerScore}`)
+
 //Set full game to play for 5 rounds
