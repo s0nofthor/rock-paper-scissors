88d36beea20a0a6d26df636b4947395785829fbe
s0nofthor <larsthomast@gmail.com>
2026-03-20 14:31:43 -0400

    Rewrite all js content for organization and clarity, add buttons to html

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,20 +1,21 @@
-//Create a scoreboard for the computer and user
+//Create scoreboard
 let humanScore = 0;
 let computerScore = 0;
 
-//Set full game to play for 5 rounds
-function playGame() {
-    //Ask user what choice they want
+function playRound() { //Tie elements of round together
     let humanChoice = prompt("Rock, paper, or scissors?", "");
-        //Have computer roll a random number between 0 and 3
     let computerChoice = Math.random() * 3;
 
-    //Take the number and assign it to a string inside a function. 0 through 0.99 is rock, 1 through 1.99 is paper, and 2 through 2.99 is scissors
-    function getComputerChoice() {
+    function getHumanChoice() { //Standardize input
+        return humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
+    };
+    console.log(getHumanChoice());
+
+    function getComputerChoice() { //Translate random number to value
         let result;
         if (computerChoice < 1) {
             result = "Rock";
-        } else if (computerChoice >= 1 && computerChoice < 2) {
+        } else if (computerChoice >= 1 && computerChoice <2) {
             result = "Paper";
         } else {
             result = "Scissors";
@@ -22,47 +23,36 @@ function playGame() {
         return result;
     };
     console.log(getComputerChoice());
-    //Log the user's choice of either rock, paper, or scissors
-    function getHumanChoice() {
-        return humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
-    };
-    console.log(getHumanChoice());
-    
-    //Compare the computer and user's choices to see who wins the round and give them the point
-    function playRound(humanChoice, computerChoice) {
-        if ((getHumanChoice() === "Rock" && getComputerChoice() === "Scissors") 
-            || (getHumanChoice() === "Paper" && getComputerChoice() === "Rock") 
+
+    function roundWinner() { //Quantify win/lose conditions
+        if ((getHumanChoice() === "Rock" && getComputerChoice() === "Scissors")
+            || (getHumanChoice() === "Paper" && getComputerChoice() === "Rock")
             || (getHumanChoice() === "Scissors" && getComputerChoice() === "Paper")) {
             result = `You win! ${getHumanChoice()} beats ${getComputerChoice()}`;
-        } else if ((getHumanChoice() === "Rock" && getComputerChoice() === "Paper") 
-            || (getHumanChoice() === "Paper" && getComputerChoice() === "Scissors") 
+            ++humanScore;
+        } else if ((getHumanChoice() === "Rock" && getComputerChoice() === "Paper")
+            || (getHumanChoice() === "Paper" && getComputerChoice() === "Scissors")
             || (getHumanChoice() === "Scissors" && getComputerChoice() === "Rock")) {
             result = `You lose! ${getComputerChoice()} beats ${getHumanChoice()}`;
+            ++computerScore;
         } else {
             result = `It's a tie!`
-        }
+        };
         return result;
     };
-    console.log(playRound());
-
-    //Give the winner a point on the scoreboard
-    if (playRound() === `You win! ${getHumanChoice()} beats ${getComputerChoice()}`) {
-        humanScore += 1;
-    } else if (playRound() === `You lose! ${getComputerChoice()} beats ${getHumanChoice()}`) {
-        computerScore += 1;
-    };
+    console.log(roundWinner());
     console.log(`User: ${humanScore} Bot: ${computerScore}`);
 };
 
-do {
-    playGame.call();
+do { //Loop gameplay for an entire game
+    playRound();
 } while (humanScore < 3 && computerScore < 3);
 
 function finalResult(humanScore, computerScore) {
     if (humanScore === 3) {
         result = "You've come out on top, treat yourself!";
     } else if (computerScore === 3) {
-        result = "You might wanna give it another shot, if you don't want to be beat by a bot";
+        result = "Give it another shot if you don't want to be beat by a bot";
     };
     return result;
 };
